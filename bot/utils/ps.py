2ddee4ca39a00c7fc28cff981dfeaf93bc10f436
Vanhbaka <vanhbaka2k7@gmail.com>
2024-11-19 01:07:11 +0700

    Update ps.py

--- bot/utils/ps.py
+++ bot/utils/ps.py
@@ -8,7 +8,7 @@ session = cloudscraper.create_scraper()
 baseUrl = "https://tonclayton.fun/api"
 pattern = r'\b\w+\s*=\s*"([^"]+)"'
 
-version = "1.0.8"
+version = "1.0.9"
 def get_main_js_format(base_url):
     try:
         response = session.get(base_url)
